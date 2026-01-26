0ac4625156f7f7b1be7932c4793a4f7227fe230e
EduBozzano <bozzanoedu@gmail.com>
2026-01-26 19:26:11 -0300

    ajuste

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, loginJwt, register, logout } from '../controllers/auth.controller.js';
+import { login, loginJwt, register, logout, logoutJWT } from '../controllers/auth.controller.js';
 import { isAuthenticated } from '../middlewares/authSession.middleware.js';
 import { loginRateLimiter } from '../middlewares/rateLimit.middleware.js';
 import { loginValidator } from '../validators/auth.validator.js';
@@ -11,5 +11,6 @@ router.post('/register', register);
 router.post('/login', loginRateLimiter, loginValidator, validateRequest, login);
 router.post('/login-jwt', loginRateLimiter, loginValidator, validateRequest, loginJwt);
 router.post('/logout', isAuthenticated, logout);
+router.post('/logout-jwt', isAuthenticated, logoutJWT);
 
 export default router;
